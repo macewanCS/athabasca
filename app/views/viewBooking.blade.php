e9ea283338fdab6f7dbeb12a0bc9e6af5ccb3c7b
Mitchell Koens <mitchkoens@shaw.ca>
2015-03-16 15:18:23 -0400

    experimentation with ajax call to show database tables

--- app/views/viewBooking.blade.php
+++ app/views/viewBooking.blade.php
@@ -1,15 +1,59 @@
 @extends('layouts.default')
 @section('content')
 
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function showBranch(str){
+var xmlhttp;    
+if (str==""){
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest){
+  xmlhttp=new XMLHttpRequest();
+  }
+else{
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
 
+xmlhttp.onreadystatechange=function(){
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","getcustomer.asp?q="+str,true);
+xmlhttp.send();
+}
+</script>
+</head>
+<body>
 <div class="container"> 
   <h3> EPL Bookings </h3> 
   <label for="branch">Branch:</label>
-  <select class="form-control" id="branch">
-    <option>EPL</option>
-    <option>Macewan</option>
-    <option>UofA</option>
-    <option>Century</option>
+  <select onchange="showBranch(this.value)" class="form-control" id="branch">
+    <option value="">Select a branch:</option>
+    <option value="EPLABB">Abbottsfield - Penny McKee Branch</option>
+    <option value="EPLCAL">Calder Branch</option>
+    <option value="EPLCPL">Capilano Branch</option>
+    <option value="EPLCSD">Castle Downs Branch</option>
+    <option value="EPLCLV">Clareview Branch</option>
+    <option value="EPLHIG">Highlands Branch</option>
+    <option value="EPLIDY">Idylwylde Branch</option>
+    <option value="EPLJPL">Jasper Place Branch</option>
+    <option value="EPLLHL">Lois Hole Library</option>
+    <option value="EPLLON">Londonderry Branch</option>
+    <option value="EPLGMU">MacEwan University Lending Machine</option>
+    <option value="EPLMEA">Meadows Branch</option>
+    <option value="EPLMLW">Mill Woods Branch</option>
+    <option value="EPLRIV">Riverbend Branch</option>
+    <option value="EPLSPW">Sprucewood Branch</option>
+    <option value="EPLMNA">Stanley A. Milner Library (Downtown)</option>
+    <option value="EPLSTR">Strathcona Branch</option>
+    <option value="EPLWMC">Whitemud Crossing Branch</option>
+    <option value="EPLWOO">Woodcroft Branch</option>
   </select>
   <table class="table table-hover">
     <thead>
@@ -42,6 +86,7 @@
     </tbody>
   </table>
 </div>
+<div id="txtHint">Customer info will be listed here...</div>
 
 </body>
 </html>
