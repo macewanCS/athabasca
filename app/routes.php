86712960aeab21127a782b7f23a44886cdd140d9
Mitchell Koens <mitchkoens@shaw.ca>
2015-03-18 19:47:52 -0400

    commit trying a different ajax method

--- app/routes.php
+++ app/routes.php
@@ -28,9 +28,9 @@ Route::post('Home', array('uses' => 'HomeController@Home'));
 Route::any('/kitmanage/create2', 'kitController@create2');
 Route::any('/kitmanage/create2add', 'kitController@create2add');
 Route::any('/createbooking', 'bookingController@check');
-Route::any('/viewbooking', 'bookingController@view');
+Route::any('/viewbooking', 'bookingController@getDatatable');
 
 Route::resource('kitmanage', 'kitController');
 Route::resource('createBooking', 'bookingController@create');
 Route::resource('ViewKits','kitViewController@create');
-Route::resource('viewBooking', 'bookingController@view');
+Route::resource('viewBooking', 'bookingController');
