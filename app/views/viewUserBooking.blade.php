072db2cf4fb51dc3fa2bf3ec7cc4e613174875ef
Mitchell Koens <mitchkoens@shaw.ca>
2015-04-11 15:35:51 -0400

    Fixed mistaken delete

--- app/views/viewUserBooking.blade.php
+++ app/views/viewUserBooking.blade.php
@@ -4,7 +4,14 @@
 <div class="container"> 
   <div class="row">
     <div class="col-md-12">
-    	<h2 style="color:#79C33A">User Bookings</h2>
+    <h2 style="color:#79C33A">User Bookings</h2>
+   	<?php if(Session::get('created',NULL)!=NULL): ?>		
+-   	<div class="alert alert-success" role="alert">		
+-        	<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>		
+-        	<span class="sr-only">Message:</span>		
+-        	{{$created}}		
+-    	</div>		
+-   <?php endif; ?>
     {{ $table->render() }}
     {{ $table->script() }}
     </div>
